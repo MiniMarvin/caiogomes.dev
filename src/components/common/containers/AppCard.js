697973ffedca96f2e29c86ae5426c67dac57e5b2
Caio Gomes <caiogomes34@gmail.com>
2020-06-17 10:04:36 -0300

    feat: apps inserted

---
+++ src/components/common/containers/AppCard.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import Img from 'gatsby-image';
+
+const Link = styled.a`
+    text-decoration: none;
+`;
+
+const Card = styled.div`
+    border-bottom-width: 1px;
+    border-bottom-color: black;
+    border-bottom-style: solid;
+    /* border-radius: 10px; */
+    width: 100%;
+    margin: 5px auto 15px;
+    padding: 5px 15px;
+    display: flex;
+    flex-direction: row;
+    cursor: pointer;
+    /* background-color: #FACD8E; */
+    /* box-shadow: 0px 0px 5px rgba(24, 40, 82, 0.5); */
+
+    h3, p {
+        margin: 0px;
+    }
+`;
+
+const ImgDiv = styled.div`
+    margin-right: 20px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+const CustomImg = styled(Img)`
+    border-radius: 5px;
+`;
+
+function AppCard(props) {
+    return (
+        <Link href={props.link} target='_blank'>
+            <Card>
+                <ImgDiv>
+                    {/* <CustomImg fixed={props.img}></CustomImg> */}
+                    {props.img ?
+                        <CustomImg fixed={props.img} /> :
+                        null
+                    }
+                </ImgDiv>
+                <div>
+                    <h3>{props.title}</h3>
+                    <p>{props.description}</p>
+                </div>
+            </Card>
+        </Link>
+    );
+}
+
+export default AppCard;
--- src/components/sections/showcase.js
+++ src/components/sections/showcase.js
@@ -4,6 +4,8 @@ import Section from './section';
 // import '../../styles/responsive-device.css';
 import { PrimaryButton } from '../common/buttons/buttons';
 import theme from '../../theme';
+import AppCard from '../common/containers/AppCard';
+import { graphql, useStaticQuery } from "gatsby";
 
 const ContentDiv = styled.div`
     margin: 0px auto;
@@ -45,21 +47,38 @@ const AppArea = styled.div`
     background-color: white;
     border-radius: 40px;
     padding: 40px 10px 0px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    span, p, h1, h2, h3, h4, h5, h6, b {
+        color: black;
+    }
 `;
 
-function smallScreen() {
+
+function smallScreen(apps) {
     return (
         <Section>
             <ContentDiv>
                 <PhoneDiv>
                     <div className="device device-iphone-x">
                         <div className="device-frame">
-                            {/* <img className="device-content" src="https://www.google.com/sky/about_files/messier82.jpg" /> */}
                             <AppArea>
-                                {/* <Spacer></Spacer> */}
-                                <PrimaryButton>
-                                    adicione seu app favorito
-                                </PrimaryButton>
+                                <div>
+                                    <h1>Outros apps</h1>
+                                    {/* <AppCard />
+                                    <AppCard />
+                                    <AppCard />
+                                    <AppCard />
+                                    <AppCard /> */}
+                                </div>
+                                <div>
+                                    <PrimaryButton>
+                                        ver todos os apps
+                                    </PrimaryButton>
+                                    <Spacer/>
+                                </div>
                             </AppArea>
                         </div>
                         <div className="device-stripe"></div>
@@ -75,18 +94,33 @@ function smallScreen() {
 }
 
 
-function bigScreen() {
+function bigScreen(apps) {
     return (
         <ShowcaseSection>
             <ContentDiv>
                 <div className="device device-iphone-x">
                     <div className="device-frame">
-                        {/* <img className="device-content" src="https://www.google.com/sky/about_files/messier82.jpg" /> */}
                         <AppArea>
-                            {/* <Spacer></Spacer> */}
-                            <PrimaryButton>
-                                adicione seu app favorito
-                            </PrimaryButton>
+                            <div>
+                                <h1>Outros apps</h1>
+                                {
+                                    apps.map((app, key) => (
+                                        <AppCard
+                                            key={key}
+                                            title={app.title}
+                                            description={app.description}
+                                            link={app.link}
+                                            img={app.img}
+                                        />
+                                    ))
+                                }
+                            </div>
+                            <div>
+                                <PrimaryButton>
+                                    ver todos os apps
+                                </PrimaryButton>
+                                <Spacer/>
+                            </div>
                         </AppArea>
                     </div>
                     <div className="device-stripe"></div>
@@ -103,9 +137,69 @@ function bigScreen() {
 
 
 function Showcase() {
+    const data = useStaticQuery(graphql`
+        query {
+            diarioAlimentar: file(relativePath: { eq: "apps/diario-alimentar.jpg" }) {
+                childImageSharp {
+                    fixed(width: 48, height: 48) {
+                        ...GatsbyImageSharpFixed
+                    }
+                }
+            }
+            cronometroEnem: file(relativePath: { eq: "apps/cronometro-enem.png" }) {
+                childImageSharp {
+                    fixed(width: 48, height: 48) {
+                        ...GatsbyImageSharpFixed
+                    }
+                }
+            }
+            moodlyMusic: file(relativePath: { eq: "apps/moodly-music.png" }) {
+                childImageSharp {
+                    fixed(width: 48, height: 48) {
+                        ...GatsbyImageSharpFixed
+                    }
+                }
+            }
+            totemClub: file(relativePath: { eq: "apps/totem-club.jpg" }) {
+                childImageSharp {
+                    fixed(width: 48, height: 48) {
+                        ...GatsbyImageSharpFixed
+                    }
+                }
+            }
+        }
+    `);
+
+    let apps = [
+        {
+            title: 'diario alimentar',
+            description: 'acompanhe a qualidade das suas refeições',
+            img: data.diarioAlimentar ? data.diarioAlimentar.childImageSharp.fixed : null,
+            link: 'https://apps.apple.com/br/app/di%C3%A1rio-alimentar/id1481634400'
+        },
+        {
+            title: 'cronometro enem',
+            description: 'simulação do cronometro da prova do enem',
+            img: data.cronometroEnem ? data.cronometroEnem.childImageSharp.fixed : null,
+            link: 'https://apps.apple.com/us/app/id1478941061'
+        },
+        {
+            title: 'moodly music',
+            description: 'músicas escolhidas a partir de seus batimentos cardíacos',
+            img: data.moodlyMusic ? data.moodlyMusic.childImageSharp.fixed : null,
+            link: 'http://bit.ly/moodlyapp'
+        },
+        {
+            title: 'totem club',
+            description: 'histórias imersivas com animações e músicas',
+            img: data.totemClub ? data.totemClub.childImageSharp.fixed : null,
+            link: 'https://apps.apple.com/br/app/totem-clube/id1463216926'
+        },
+    ];
+
     return (
         <>
-            {window.innerWidth < theme.limitSize ? smallScreen() : bigScreen()}
+            {window.innerWidth < theme.limitSize ? smallScreen(apps) : bigScreen(apps)}
         </>
     );
 }

ec056c2580a864d3c7dad4fccfb6420e9b0eba73
Caio Gomes <caiogomes34@gmail.com>
2020-06-18 13:44:50 -0300

    fix: project

--- src/components/sections/project.js
+++ src/components/sections/project.js
@@ -6,151 +6,176 @@ import '../../styles/picturepan2/devices.min.css';
 import '../../styles/responsive-device.css';
 import { SecondaryButton, PrimaryButton } from '../common/buttons/buttons';
 import withSizes from 'react-sizes';
-
-const limitSize = 600;
+import { MobileOnlyDiv, DesktopOnlyDiv } from '../common/containers/MobileRender';
 
 const ContentDiv = styled.div`
-    margin: 0px auto;
-    display: flex;
-    flex-direction: column;
+  margin: 0px auto;
+  display: flex;
+  flex-direction: column;
 
-    p {
-        font-size: 18px;
-    }
+  p {
+    font-size: 18px;
+  }
 `;
 
 const PhoneDiv = styled.div`
-    /* max-height: 200px; */
-    height: 415px;
-    padding: 0px calc(50% - 98.44px);
+  /* max-height: 200px; */
+  height: 415px;
+  padding: 0px calc(50% - 98.44px);
 `;
 const ButtonDiv = styled.div`
-    z-index: 20;
-    margin: 0px auto;
+  z-index: 20;
+  margin: 0px auto;
+
+  @media (max-width: ${theme.limitSize}px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 const FlexContent = styled.div`
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-    p {
-        font-size: 18px;
-    }
+  p {
+    font-size: 18px;
+  }
 `;
 
 const Partial = styled.div`
-    display: flex;
-    flex-direction: ${props => props.direction === 'reverse' ? 'row-reverse' : 'row'};
+  display: flex;
+  flex-direction: ${props => props.direction === 'reverse' ? 'row-reverse' : 'row'};
+  @media (max-width: ${theme.limitSize}px) {
+    flex-direction: column;
+  }
 `;
 
 const PartialCol = styled.div`
-    width: 100%;
-    overflow: hidden;
-    a {
-        margin-top: 50px;
-    }
+  width: 100%;
+  overflow: hidden;
+  a {
+    margin-top: 50px;
+  }
 `;
 
 const Title = styled.div`
-    font-size: 36px;
+  font-size: 36px;
+  @media (max-width: ${theme.limitSize}px) {
+    text-align: left;
+    width: 100%;
+  }
 `;
 
 const Description = styled.p`
-    margin-bottom: 50px;
+  margin-bottom: 50px;
 `;
 
 const ReduceAMargin = styled.div`
-    a {
-        margin: 10px 0px 0px 0px;
-    }
-`
+  a {
+    margin: 10px 0px 0px 0px;
+  }
+`;
+
+const Spacer = styled.div`
+  height: 20px;
+`;
 
 function smallScreen(props) {
-    return(
-        <div>
-            <Section>
-                <ContentDiv>
-                    <PhoneDiv>
-                        <div className="device device-iphone-8">
-                            <div className="device-frame">
-                                {props.children}
-                            </div>
-                            <div className="device-stripe"></div>
-                            <div className="device-header"></div>
-                            <div className="device-sensors"></div>
-                            <div className="device-btns"></div>
-                            <div className="device-power"></div>
-                        </div>
-                    </PhoneDiv>
-                    <ButtonDiv>
-                        <SecondaryButton href={props.link} target='_blank' width='300px'>acesse agora o aplicativo</SecondaryButton>
-                    </ButtonDiv>
-                    <div></div>
-                </ContentDiv>
-            </Section>
-            <Section>
-                <FlexContent>
-                    <Title>{props.title}</Title>
-                    <Description>{props.description}</Description>
-                    <PrimaryButton href='https://forms.gle/pXbtucfzxaQhSQV26'>
-                        {props.ctaTitle}
-                    </PrimaryButton>
-                </FlexContent>
-            </Section>
-        </div>
-    );
+  return(
+    <div>
+      <Section>
+        <MobileOnlyDiv>
+          <FlexContent>
+            <Title>{props.title}</Title>
+          </FlexContent>
+        </MobileOnlyDiv>
+        <ContentDiv>
+          <PhoneDiv>
+            <div className="device device-iphone-8">
+              <div className="device-frame">
+                {props.children}
+              </div>
+              <div className="device-stripe"></div>
+              <div className="device-header"></div>
+              <div className="device-sensors"></div>
+              <div className="device-btns"></div>
+              <div className="device-power"></div>
+            </div>
+          </PhoneDiv>
+          <ButtonDiv>
+            <SecondaryButton href={props.link} target='_blank' width='300px'>acesse agora o aplicativo</SecondaryButton>
+          </ButtonDiv>
+          <div></div>
+        </ContentDiv>
+      </Section>
+      <Section>
+        <FlexContent>
+          <Title>{props.title}</Title>
+          <Description>{props.description}</Description>
+          <PrimaryButton href='https://forms.gle/pXbtucfzxaQhSQV26'>
+            {props.ctaTitle}
+          </PrimaryButton>
+        </FlexContent>
+      </Section>
+    </div>
+  );
 }
-
-function bigScreen(props) {
-    return(
-        <Partial direction={props.direction} id={props.id}>
-            <PartialCol>
-                <Section>
-                    <ContentDiv>
-                        <PhoneDiv>
-                            <div className="device device-iphone-8">
-                                <div className="device-frame">
-                                    {props.children}
-                                </div>
-                                <div className="device-stripe"></div>
-                                <div className="device-header"></div>
-                                <div className="device-sensors"></div>
-                                <div className="device-btns"></div>
-                                <div className="device-power"></div>
-                            </div>
-                        </PhoneDiv>
-                        <div></div>
-                    </ContentDiv>
-                </Section>
-            </PartialCol>
-            <PartialCol>
-                <Section>
-                    <FlexContent>
-                        <Title>{props.title}</Title>
-                        <Description>{props.description}</Description>
-                        <ButtonDiv>
-                            <SecondaryButton href={props.link} target='_blank' width='300px'>acesse agora o aplicativo</SecondaryButton>
-                        </ButtonDiv>
-                        <ReduceAMargin>
-                            <PrimaryButton href='https://forms.gle/pXbtucfzxaQhSQV26'>
-                                {props.ctaTitle}
-                            </PrimaryButton>
-                        </ReduceAMargin>
-                    </FlexContent>
-                </Section>
-            </PartialCol>
-        </Partial>
-    );
-}
-
 
 function Project(props) {
-
-    let renderSmall = props.isSmall;
-
-    return(
-        <>{renderSmall ? smallScreen(props) : bigScreen(props)}</>
-    );
+  return(
+  <Partial direction={props.direction} id={props.id}>
+    <PartialCol>
+      <Section>
+        <MobileOnlyDiv>
+          <FlexContent>
+            <Title>{props.title}</Title>
+            <Spacer />
+          </FlexContent>
+        </MobileOnlyDiv>
+        <ContentDiv>
+          <PhoneDiv>
+            <div className="device device-iphone-8">
+              <div className="device-frame">
+                {props.children}
+              </div>
+              <div className="device-stripe"></div>
+              <div className="device-header"></div>
+              <div className="device-sensors"></div>
+              <div className="device-btns"></div>
+              <div className="device-power"></div>
+            </div>
+          </PhoneDiv>
+          <MobileOnlyDiv>
+            <ButtonDiv>
+              <SecondaryButton href={props.link} target='_blank' width='300px'>acesse agora o aplicativo</SecondaryButton>
+            </ButtonDiv>
+          </MobileOnlyDiv>
+        </ContentDiv>
+      </Section>
+    </PartialCol>
+    <PartialCol>
+      <Section>
+        <FlexContent>
+          <DesktopOnlyDiv>
+            <Title>{props.title}</Title>
+          </DesktopOnlyDiv>
+          <Description>{props.description}</Description>
+          <DesktopOnlyDiv>
+            <ButtonDiv>
+              <SecondaryButton href={props.link} target='_blank' width='300px'>acesse agora o aplicativo</SecondaryButton>
+            </ButtonDiv>
+            <ReduceAMargin>
+              <PrimaryButton href='https://forms.gle/pXbtucfzxaQhSQV26'>
+                {props.ctaTitle}
+              </PrimaryButton>
+            </ReduceAMargin>
+          </DesktopOnlyDiv>
+        </FlexContent>
+      </Section>
+    </PartialCol>
+  </Partial>
+  );
 }
 
 const mapSizesToProps = sizes => ({
